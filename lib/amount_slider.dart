796d8c0e15977993cf0d82976a2b9bd24a7d4dfc
Maksym Kovalenko <maksym.k@etheric.agency>
2021-09-20 15:14:58 +0200

    Implement increasing width of divisions when coming closer to center

--- lib/amount_slider.dart
+++ lib/amount_slider.dart
@@ -1,9 +1,8 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 
 const divisionsCount = 16;
-const divisionWidth = 2.0;
+const divisionWidth = 3.0;
+const mainDivisionWidth = 4.0;
 const spaceBetweenDivisions = 40.0;
 const divisionMaxHeight = 70.0;
 
@@ -40,11 +39,11 @@ class _AmountSliderState extends State<AmountSlider> {
     scrollController.addListener(() {
       setState(() {
         if (widget.onChanged != null) {
-          if (scrollController.offset < 0) {
+          if (scrollController.offset <= 0) {
             widget.onChanged!(widget.min);
             return;
           }
-          if (scrollController.offset > scrollWidth) {
+          if (scrollController.offset >= scrollWidth) {
             widget.onChanged!(widget.max);
             return;
           }
@@ -86,20 +85,19 @@ class _AmountSliderState extends State<AmountSlider> {
                 itemBuilder: (context, index) {
                   if (index == 0 || index == divisionsCount + 1) {
                     return SizedBox(
-                      width: xMiddleScreen - spaceBetweenDivisions - (divisionWidth / 2),
+                      width: xMiddleScreen -
+                          spaceBetweenDivisions -
+                          (mainDivisionWidth / 2),
                     );
                   }
                   return AnimatedContainer(
                     margin: EdgeInsets.symmetric(
-                      vertical: calculateDivisionPadding(
-                        scrollController.position.pixels,
-                        index,
-                      ),
+                      vertical: calculateDivisionPadding(index),
                     ),
-                    width: divisionWidth,
+                    width: calculateDivisionWidth(index),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(2),
-                      color: Colors.grey,
+                      color: const Color(0x80FFFFFF),
                     ),
                     duration: const Duration(microseconds: 100),
                   );
@@ -110,7 +108,7 @@ class _AmountSliderState extends State<AmountSlider> {
         ),
         Container(
           height: divisionMaxHeight,
-          width: divisionWidth,
+          width: mainDivisionWidth,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(2),
             color: Colors.white,
@@ -120,30 +118,41 @@ class _AmountSliderState extends State<AmountSlider> {
     );
   }
 
-  double calculateDivisionPadding(double scrollPosition, int itemIndex) {
-    final newItemIndex = itemIndex + 1;
-    final itemStartPosition = (newItemIndex * divisionWidth +
-            (newItemIndex - 1) * spaceBetweenDivisions) +
-        xMiddleScreen -
-        spaceBetweenDivisions;
-    var itemDistanceToCenter;
-    if (itemStartPosition < scrollPosition) {
-      itemDistanceToCenter =
-          (xMiddleScreen - itemStartPosition - scrollPosition).abs();
-    } else {
-      itemDistanceToCenter =
-          (xMiddleScreen - itemStartPosition + scrollPosition).abs();
-    }
-
+  double calculateDivisionPadding(int itemIndex) {
     final normalMargin = (divisionMaxHeight - 10) / 2;
-
+    final itemDistanceToCenter = calculateDistanceToCenter(itemIndex).abs();
     if (itemDistanceToCenter > distanceFromCenterToStartTransform) {
       return normalMargin;
     } else {
-      final marginSqrt = math.sqrt(itemDistanceToCenter) /
-          math.sqrt(distanceFromCenterToStartTransform) *
-          math.sqrt(normalMargin);
-      return marginSqrt * marginSqrt;
+      final margin = itemDistanceToCenter /
+          distanceFromCenterToStartTransform *
+          normalMargin;
+      return margin;
     }
+  }
+
+  double calculateDistanceToCenter(int itemIndex) {
+    final scrollPosition = scrollController.position.pixels;
+    final divisionsWidth = itemIndex * divisionWidth;
+    final itemStartPosition =
+        (divisionsWidth + itemIndex * spaceBetweenDivisions) +
+            (xMiddleScreen - spaceBetweenDivisions - (divisionWidth / 2));
+    if (itemStartPosition < scrollPosition) {
+      return xMiddleScreen - itemStartPosition - scrollPosition;
+    } else {
+      return xMiddleScreen - itemStartPosition + scrollPosition;
+    }
+  }
+
+  double calculateDivisionWidth(int itemIndex) {
+    final itemDistanceToCenter = calculateDistanceToCenter(itemIndex);
+    if (itemDistanceToCenter >= -40 - mainDivisionWidth / 2 &&
+        itemDistanceToCenter < 0) {
+      return mainDivisionWidth;
+    } else if (itemDistanceToCenter <= 40 + mainDivisionWidth / 2 &&
+        itemDistanceToCenter > 0) {
+      return mainDivisionWidth;
+    }
+    return divisionWidth;
   }
 }
